9a157f720b623cad4bc58efb6cd70d13d745534b
ThamiresMarina <thamiresmarina@gmail.com>
2026-04-18 13:34:31 +0100

    fix: set headless true for CI

--- tests/hooks/hooks.js
+++ tests/hooks/hooks.js
@@ -2,13 +2,20 @@ const { Before, After, setDefaultTimeout } = require('@cucumber/cucumber');
 const { chromium } = require('playwright');
 require('dotenv').config();
 
-setDefaultTimeout(60000); 
+setDefaultTimeout(120000);
 
 Before(async function () {
-  this.browser = await chromium.launch({ headless: false });
-  this.page = await this.browser.newPage();
+  this.browser = await chromium.launch({ headless: true });
+  const context = await this.browser.newContext({
+    locale: 'en-US'
+  });
+  this.page = await context.newPage();
 });
 
-After(async function () {
-  await this.browser.close();
-});
+After(async function (scenario) {
+    if (scenario.result.status === 'FAILED') {
+        const screenshot = await this.page.screenshot()
+        await this.attach(screenshot, 'image/png')
+    }
+    await this.browser.close()
+})
