1937ee14a05e4cd5f15845c470b583db50b55ec4
diegovalter <di_valter@hotmail.com>
2026-04-19 16:03:06 -0300

    refactor: ajuste na lógica da page

---
+++ pages/AdminPage.js
@@ -0,0 +1,21 @@
+class AdminPage {
+    constructor(page) {
+    this.page = page
+    this.abaPlugins = page.locator('[data-text="Plugins"]')
+    this.abaAparencia = page.locator('[data-text="Aparência"]')
+    this.linkTemaAdmin = page.getByRole('link', { name: 'Gerenciamento de Temas' });
+    this.linkTemaCliente = page.getByRole('link', { name: 'Gerenciar Temas' });
+
+    }
+
+    async acessarGerenciamentoTemas(usuario) {
+        if (usuario === 'admin') {
+            await this.abaAparencia.click();
+            await this.linkTemaAdmin.click();
+        } else {
+            await this.linkTemaCliente.click();
+        }
+    }
+}
+
+module.exports = {AdminPage}
